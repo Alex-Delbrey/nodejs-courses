7e4009998fdd2bd0a1df864ca1add79286b528cc
Jandro <jandro@jandro.localdomain>
2024-12-27 15:24:43 -0400

    playing with fs

---
+++ src/notes.js
@@ -0,0 +1,16 @@
+import { insertDB, saveDB, getDB } from "./db";
+
+export const newNote = async (note, tags) => {
+  const newNote = {
+    tags,
+    id: Date.now(),
+    content: note,
+  }
+  await insertDB(newNote)
+  return newNote
+}
+
+export const getAllNotes = async () => {
+  const { notes } = await getDb()
+  return notes
+}
